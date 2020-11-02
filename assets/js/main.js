6b1dd6e08abc1f3cbda5fe18bfca6122e8761123
Samson Akanbi <samson.akanbi@outlook.com>
2020-11-02 23:25:17 +0100

    Created variables for the menus.

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+// MENU SHOW Y HIDDEN
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
