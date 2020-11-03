ac177c7f8dd145da7fce6de1659b46dd460090c6
Samson Akanbi <samson.akanbi@outlook.com>
2020-11-03 22:06:20 +0100

    Linked the show event listener.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,8 @@
 const navMenu = document.getElementById('nav-menu'),
     toggleMenu = document.getElementById('nav-toggle'),
     closeMenu = document.getElementById('nav-close')
+
+// SHOW
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
