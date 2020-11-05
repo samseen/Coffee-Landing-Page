243ea078a05a849fee3cfbd20ee3c595b5092f34
Samson Akanbi <samson.akanbi@outlook.com>
2020-11-05 22:22:50 +0100

    Gave values to the data-speed attributes in the home page.

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,5 +17,12 @@ closeMenu.addEventListener('click', ()=>{
 document.addEventListener('mousemove', move)
 
 function move(e) {
-    this.querySelectorAll('.move')
+    this.querySelectorAll('.move').forEach(layer =>{
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX*speed)/120;
+        const y = (window.innerWidth - e.pageY*speed)/120;
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
 }
