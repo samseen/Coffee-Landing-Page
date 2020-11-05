27eba2d11220507a123ace570a14d29fb268de9e
Samson Akanbi <samson.akanbi@outlook.com>
2020-11-05 22:39:01 +0100

    Delayed the home image.

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,4 @@ gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y:30, stagger: 0.2
 gsap.from('.home__title', {opacity: 0, duration: 1, delay: 1.6, y:30})
 gsap.from('.home__description', {opacity: 0, duration: 1, delay: 1.8, y:30})
 gsap.from('.home__button', {opacity: 0, duration: 1, delay: 2.1, y:30})
+gsap.from('.home__img', {opacity: 0, duration: 1, delay: 1.3, y:30})
