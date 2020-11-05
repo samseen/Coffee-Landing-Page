e2549cab3158cb9aff1d35cfdabc3a9ef18d3ff1
Samson Akanbi <samson.akanbi@outlook.com>
2020-11-05 22:38:00 +0100

    Delayed the home button

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,8 @@ function move(e) {
 // NAV
 gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay: 2, y:10})
 gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y:30, stagger: 0.2})
+
+// HOME
+gsap.from('.home__title', {opacity: 0, duration: 1, delay: 1.6, y:30})
+gsap.from('.home__description', {opacity: 0, duration: 1, delay: 1.8, y:30})
+gsap.from('.home__button', {opacity: 0, duration: 1, delay: 2.1, y:30})
